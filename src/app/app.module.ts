78ec9dc1e0591f13078e6e60241ad30c5c5cedd7
mbellange <mbellange@gmail.com>
2019-06-12 15:30:28 +0200

    refacto file directory of store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { EffectsModule } from '@ngrx/effects';
-import { AppEffects } from './app.effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { AppEffects, reducers, metaReducers } from './store';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ExpensesModule } from './expenses/expenses.module';
 
---
+++ src/app/store/index.ts
@@ -0,0 +1,4 @@
+import { AppEffects } from './app.effects';
+import { reducers, metaReducers } from './reducers';
+
+export { AppEffects, reducers, metaReducers };
