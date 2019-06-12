293f19f13a1fa7a2487d5bea48a716aa6d98149f
mbellange <mbellange@gmail.com>
2019-06-12 15:03:26 +0200

    add default routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/expenses',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/expenses',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
