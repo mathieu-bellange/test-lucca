7bff4580d3a89f802cc4b569647fe1f765a62596
mbellange <mbellange@gmail.com>
2019-06-13 18:10:21 +0200

    refactor delete unnecessary files

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,8 +1,7 @@
 import * as ExpensesStore from './expenses';
-import { reducers, metaReducers } from './reducers';
 
 export interface AppState {
   expenses: ExpensesStore.State;
 }
 
-export { ExpensesStore, reducers, metaReducers };
+export { ExpensesStore };
