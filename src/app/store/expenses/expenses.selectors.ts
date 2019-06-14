7076dd725b4bf99edcae4efe61e5d5d31b8bec30
mbellange <mbellange@gmail.com>
2019-06-14 21:36:26 +0200

    add selector for expense item by id

--- src/app/store/expenses/expenses.selectors.ts
+++ src/app/store/expenses/expenses.selectors.ts
@@ -2,10 +2,16 @@ import { createSelector } from '@ngrx/store';
 
 import { State } from './expenses.state';
 import { AppState } from '../index';
+import { ExpenseItem } from './model';
 
 export const selectExpenses = (state: AppState) => state.expenses;
 
 export const selectExpenseItems = createSelector(
   selectExpenses,
   (state: State) => state.entities
+);
+
+export const selectExpenseItemById = createSelector(
+  selectExpenseItems,
+  (entities: { [id: string]: ExpenseItem }, id: string) => entities[id]
 );
